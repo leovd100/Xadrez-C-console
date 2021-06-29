7e73d6476901d13e2bb17061fafd8a0a0fcdfc22
Leonardo <demetrio.vd@hotmail.com>
2021-06-29 13:46:30 -0300

    Tratando erros com execao personalizada

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -7,14 +7,21 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 5));
-            tab.colocarPeca(new Rei(tab, Cor.Azul), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(9, 5));
+                tab.colocarPeca(new Rei(tab, Cor.Azul), new Posicao(0, 2));
 
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
         }
     }
 }
